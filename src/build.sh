5028279d503473a89fade54c8cb9548d1a0881ed
Aaron Gonzales <aaron@aarongonzales.net>
2023-12-11 00:51:51 -0700

    updates

--- src/build.sh
+++ src/build.sh
@@ -1,41 +1,49 @@
+#!/bin/bash
 # prerequsites
 # helpful gist https://gist.github.com/peterhurford/75957ba9335e755013b87254ec85fab1
 
 brew_cask_install() {
-    echo "\nInstalling $1"
-    if brew list $1 &>/dev/null; then
+    printf "\nInstalling %s" "$1"
+    if brew list "$1" &>/dev/null; then
         echo "${1} is already installed"
     else
         brew install --cask $1 && echo "$1 is installed"
     fi
 }
 brew_install() {
-    echo "\nInstalling $1"
-    if brew list $1 &>/dev/null; then
+    printf "\n Installing %s" "$1"
+    if brew list "$1" &>/dev/null; then
         echo "${1} is already installed"
     else
-        brew install $1 && echo "$1 is installed"
+        brew install "$1" && echo "$1 is installed"
     fi
 }
 
 brew_cask_install basictex
+eval "$(/usr/libexec/path_helper)"
+# Update $PATH to include `/usr/local/texlive/2022basic/bin/universal-darwin`
 brew_install biber
 brew tap homebrew/cask-fonts
 brew_cask_install font-hack
 
 sudo tlmgr update --self
-sudo tlmgr install texliveonfly 
-sudo tlmgr install xelatex
-sudo tlmgr install adjustbox
-sudo tlmgr install tcolorbox
-sudo tlmgr install collectbox
-sudo tlmgr install ucs
-sudo tlmgr install environ
-sudo tlmgr install trimspaces
-sudo tlmgr install titling
-sudo tlmgr install enumitem
-sudo tlmgr install rsfs
-sudo tlmgr install biblatex
-sudo tlmgr install biber
+sudo tlmgr install texliveonfly  \
+xelatex \
+adjustbox \
+tcolorbox \
+collectbox \
+ucs \
+environ \
+trimspaces \
+titling \
+enumitem \
+rsfs \
+biblatex \
+biber \
+chktex \
+latexindent \
+marvosym \
+biblatex-ieee \
+lastpage
 
 
